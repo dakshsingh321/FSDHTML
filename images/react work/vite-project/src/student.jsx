251dcb0d4cd3d7e800c30cf91a572dbd304e0be8
dakshsingh321 <dakshitsingh328@gmail.com>
2024-12-11 13:10:42 +0530

    image manupulation

---
+++ images/react work/vite-project/src/student.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style.css'
+function student(props){
+    return(
+        <div classname='icard'>
+            <h2>student info</h2>
+            <h3>
+                <name:>props.name</name:>
+                <roll:>props.roll</roll:>
+            </h3>
+        </div>
+    )
+}
